72d4e7f611eb2a1486cf273f12ff9691acbcfc70
Nadav09088 <nadavy6666@gmail.com>
2024-01-26 11:55:13 +0200

    changed

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		for(int i=1;i<=10;i++){
-			System.out.println("This is First job(Hello-job)"+i);
+			System.out.println("This is First job(changed)"+i);
 		}
 	}
 }
